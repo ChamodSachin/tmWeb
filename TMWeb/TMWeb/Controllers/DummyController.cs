737a4a53bd6ba614d9fb5fb339475b068c6e4348
ChamodSachin <cspg1997@gmail.com>
2019-04-22 18:01:15 +0530

    commit 1

---
+++ TMWeb/TMWeb/Controllers/DummyController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TMWeb.Controllers
+{
+    public class DummyController : ApiController
+    {
+
+        /* Fuction: getImage
+         * Type: Get
+         * Parameters: zoom level, x coordinate, y coordinate
+         * Return: byte[]
+         * Assertion:  function returns a dummy image, regardless of input.
+         */
+        [HttpGet]
+        [Route("TMWeb/{zoom}/{x}/{y}")]
+        public byte[] Get(int zoom, int x, int y)
+        {
+            byte[] imageBuf = System.IO.File.ReadAllBytes("Resources/LoremIpsum.jpg");
+            return imageBuf;
+        }
+
+        /* Fuction: getImage
+        * Type: Post
+        * Parameters: zoom level, x coordinate, y coordinate
+        * Return: byte[]
+        * Assertion: function returns a dummy image, regardless of input.
+        * Note: Post version of the previous method.
+        */
+        [HttpPost]
+        [Route("TMWeb")]
+        public byte[] Post([FromBody]Dictionary<string, object> req)
+        {
+            byte[] imageBuf = System.IO.File.ReadAllBytes("Resources/LoremIpsum.jpg");
+            return imageBuf;
+        }
+
+        /* Fuction: zoom
+         * Type: Get
+         * Parameters: -
+         * Return: the number of zoom levels
+         */
+        [HttpGet]
+        [Route("TMWeb/Zoom")]
+        public int Get()
+        {
+            return 6;
+        }
+
+        /* Fuction: zoom
+         * Type: Get
+         * Parameters: zoom level
+         * Return: zoom: the
+         *         across:
+         *         down:
+         * function: will validate the zoom, and return the
+         *           number of tiles across and down
+         */
+        [HttpGet]
+        [Route("TMWeb/zoom/{Zoom}")]
+        public Dictionary<string, int> Get(int zoom)
+        {
+            Dictionary<string, int> ret = new Dictionary<string, int>();
+            ret.Add("across", 1);
+            ret.Add("down", 1);
+
+            return ret;
+        }
+    }
+}
